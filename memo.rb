1e614f81cf192fd0199c1aab1da27fa1e2c8a456
ryuya13579 <gesuyabami@icloud.com>
2022-03-24 15:36:23 +0900

    memo

---
+++ memo.rb
@@ -0,0 +1,47 @@
+  require "csv" 
+  
+  puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+  
+  memo_type = gets.chomp
+
+  if memo_type == "1" 
+    puts "拡張子を除いたファイルを入力してください"    
+  elsif memo_type == "2"
+    puts "拡張子を除いた既存ファイルを入力してください"
+  end 
+
+  file_name = gets.chomp
+
+  if memo_type == "1"
+ CSV.open(file_name + '.csv',"w")do |csv|
+  end
+  ##引数のwで新規作成(上書き)になる　aなら末尾に追加される
+  ##memo_type + '.csv' で変数に文字列を連結させる
+  elsif memo_type == "2"
+ CSV.open(file_name + '.csv',"a")do |csv|
+  end
+end
+
+puts "メモしたい内容を入力してください"
+puts "完了したらCtrl + D を押します"
+
+file_text = gets.to_s
+
+if memo_type == "1"
+ CSV.open(file_name + '.csv','w')do |csv|
+ csv << [file_text]
+ puts "作成しました"
+ end
+
+
+elsif memo_type == "2"
+ CSV.open(file_name + '.csv','a+')do |csv|
+ csv << [file_text]
+ puts "追加しました"
+ end
+
+end
+
+
+
+  ##メモの内容を複数行できるようにしてエンターで改行？、同時コマンドで終了とする
